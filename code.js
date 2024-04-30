ae58c59b10f6c91a5d050e558d3c8000b277a665
reis-iken <144738295+reis-iken@users.noreply.github.com>
2024-04-30 13:00:54 -0600

    code.js

--- code.js
+++ code.js
@@ -1,3 +1,17 @@
 function depthFirstSearch(graph, startNode, targetNode) {
+    let visited = new visitedSet();
+    let stack = [[startNode, [startNode]]];
+    while (stack.length > 0) {
+        let [currentNode, path] = stack.pop();
+        if (currentNode === targetNode) {
+            return path;
+        }
+        visited.add(currentNode);
+        for (let neighbor of graph[currentNode]) {
+            if (!visited.has(neighbor)) {
+                stack.push([neighbor, path.concat(neighbor)]);
+            }
+        }
+    }
     return [];
 }
