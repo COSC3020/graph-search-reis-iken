0a2ba8d5679edfb0261e731e43843a9a65a3176d
reis-iken <144738295+reis-iken@users.noreply.github.com>
2024-04-30 15:43:54 -0600

    code.test.js

--- code.test.js
+++ code.test.js
@@ -1,13 +1,14 @@
 let fs = require('fs');
 let jsc = require('jsverify');
 eval(fs.readFileSync('code.js')+'');
-
 let testDepthFirstSearch = jsc.forall(jsc.dict(jsc.array(jsc.string)), jsc.nestring, jsc.nestring, function(graph, startNode, targetNode) {
     let result = depthFirstSearch(graph, startNode, targetNode);
-
     function isValidPath(path) {
         if (path.length === 0) {
             return startNode === targetNode;
+        }
+        if (path[0] !== startNode || path[path.length - 1] !== targetNode) {
+            return false;
         }
         for (let i = 0; i < path.length - 1; i++) {
             if (!graph[path[i]] || !graph[path[i]].includes(path[i + 1])) {
@@ -16,8 +17,7 @@ let testDepthFirstSearch = jsc.forall(jsc.dict(jsc.array(jsc.string)), jsc.nestr
         }
         return true;
     }
-
     return isValidPath(result);
-}
+});
 
 jsc.assert(testDepthFirstSearch);
