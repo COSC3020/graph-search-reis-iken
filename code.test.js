9c589b25c205691e9b98d81a99ee878b830d0f88
reis-iken <144738295+reis-iken@users.noreply.github.com>
2024-05-01 16:37:16 -0600

    code.test.js

--- code.test.js
+++ code.test.js
@@ -2,16 +2,18 @@ let fs = require('fs');
 let jsc = require('jsverify');
 eval(fs.readFileSync('code.js')+'');
 
-let testDepthFirstSearch = jsc.forall("json", "nat", "nat", function (graph, startNode, targetNode) {
-
+let testDepthFirstSearch = jsc.forall(jsc.array(jsc.array(jsc.nat())), jsc.nat(), jsc.nat(), function (graph, startNode, targetNode) {
+    if (graph.length === 0 || startNode >= graph.length || targetNode >= graph.length) {
+        return true;
+    }
     let solutions = {
-        "((0, 1, 2, 3, 4), 0, 4)": [0, 1, 2, 3, 4],
-        "((0, 1, 2, 3, 4), 3, 0)": [3, 0],
-        "((0, 1, 2, 3, 4), 1, 5)": [],
+        "([[0, 1, 2, 3, 4]], 0, 4)": [0, 1, 2, 3, 4],
+        "([[0, 1, 2, 3, 4]], 3, 0)": [3, 0],
+        "([[0, 1, 2, 3, 4]], 1, 5)": [],
     };
 
     let result = depthFirstSearch(graph, startNode, targetNode);
-    let expected = solutions[[graph, startNode, targetNode]] || [];
+    let expected = solutions[JSON.stringify([graph, startNode, targetNode])] || [];
 
     return jsc.utils.isEqual(result, expected);
 });
