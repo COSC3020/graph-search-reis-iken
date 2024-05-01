cb88298e37963c7e665ac0481572e86cb9019219
reis-iken <144738295+reis-iken@users.noreply.github.com>
2024-05-01 17:24:06 -0600

    code.js

--- code.js
+++ code.js
@@ -1,21 +1,20 @@
 function depthFirstSearch(adjMatrix, startNode, targetNode) {
     if (!adjMatrix || !adjMatrix.length) { return []; }
-    let visited = []; 
+    let visited = new Array(adjMatrix.length).fill(false); 
     
     function dfsFinder(currentNode, path) {
-        visited.push(currentNode);
-        if (currentNode == targetNode) { return path.concat(currentNode); }
+        visited[currentNode] = true;
+        if (currentNode === targetNode) { return [...path, currentNode]; }
         for (let adj = 0; adj < adjMatrix[currentNode].length; adj++) {
-            if (!visited.includes(adj) && adjMatrix[currentNode][adj] == 1) {
-                let foundPath = dfsFinder(adj, path.concat(currentNode));
-                if (foundPath) { return foundPath; }
+            if (!visited[adj] && adjMatrix[currentNode][adj] === 1) {
+                let foundPath = dfsFinder(adj, [...path, currentNode]);
+                if (foundPath.length > 0) { return foundPath; }
             }
         }
         return [];
     }
     
-    let solution = dfsFinder(startNode, []);
-    return solution;
+    return dfsFinder(startNode, []);
 }
 
 module.exports = depthFirstSearch;
