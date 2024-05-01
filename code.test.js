19dd424572a6263afbede4b0e6b039a761d935ca
reis-iken <144738295+reis-iken@users.noreply.github.com>
2024-05-01 17:32:47 -0600

    code.test.js

--- code.test.js
+++ code.test.js
@@ -1,4 +1,3 @@
-
 const fs = require('fs');
 const assert = require('assert');
 
@@ -6,67 +5,27 @@ eval(fs.readFileSync('code.js')+'');
 
 const testCases = [
   {
-    description: 'Empty adjacency matrix',
-    adjacencyMatrix: [],
-    startNode: 0,
-    targetNode: 1,
-    expectedPath: [],
-  },
-
-  {
-    description: 'Single node graph (no path possible)',
-    adjacencyMatrix: [[0]],
-    startNode: 0,
-    targetNode: 1,
-    expectedPath: [],
-  },
-
-  {
-    description: 'Disconnected graph (no path possible)',
-    adjacencyMatrix: [[0, 0], [0, 0]],
-    startNode: 0,
-    targetNode: 1,
-    expectedPath: [],
-  },
-
-  {
-    description: 'Simple connected graph (path exists)',
-    adjacencyMatrix: [[0, 1], [1, 0]],
-    startNode: 0,
-    targetNode: 1,
-    expectedPath: [0, 1],
-  },
-
-  {
-    description: 'Graph with multiple paths (should find one)',
+    description: 'Graph with a loop',
     adjacencyMatrix: [[0, 1, 0], [1, 0, 1], [0, 1, 0]],
-    startNode: 0,
-    targetNode: 2,
-    expectedPath: [0, 1, 2],
-  },
-
-  {
-    description: 'Target Node is Starting Node',
-    adjacencyMatrix: [[1, 0], [0, 1]],
     startNode: 0,
     targetNode: 0,
     expectedPath: [0],
   },
 
   {
-    description: 'Target Node unreachable (no path possible)',
-    adjacencyMatrix: [[0, 1, 0], [0, 0, 0], [0, 0, 1]],
+    description: 'Graph with a longer path',
+    adjacencyMatrix: [[0, 1, 1, 0], [1, 0, 1, 0], [1, 1, 0, 1], [0, 0, 1, 0]],
     startNode: 0,
-    targetNode: 2,
-    expectedPath: [],
+    targetNode: 3,
+    expectedPath: [0, 2, 3],
   },
 
   {
-    description: 'Directed graph with a path',
-    adjacencyMatrix: [[0, 1, 0], [0, 0, 1], [0, 0, 0]],
+    description: 'Graph with multiple disconnected components',
+    adjacencyMatrix: [[0, 1, 0, 0], [1, 0, 0, 0], [0, 0, 0, 1], [0, 0, 1, 0]],
     startNode: 0,
-    targetNode: 2,
-    expectedPath: [0, 1, 2],
+    targetNode: 3,
+    expectedPath: [0, 1, 3],
   },
 ];
 
@@ -84,5 +43,5 @@ function runTest(testCase) {
 }
 
 for(let c = 0; c < testCases.length; c++){
-	assert(runTest(testCases[c]));
+  assert(runTest(testCases[c]));
 }
