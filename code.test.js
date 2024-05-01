aac83e8227e5e4313cc9042bf60c0437f831d4a3
reis-iken <144738295+reis-iken@users.noreply.github.com>
2024-05-01 17:18:20 -0600

    code.test.js

--- code.test.js
+++ code.test.js
@@ -3,15 +3,11 @@ const assert = require('assert');
 
 eval(fs.readFileSync('code.js')+'');
 
-a = [[0,1,2],[3,2,1],[1,2],[1,2,3]]
-b = [[3],[6],[2,3,4],[3,4,5],[4,5,6],[5,6,7],[6,7,8],[7,0,1]]
+const a = [[0,1,2],[3,2,1],[1,2],[1,2,3]];
+const b = [[0,1,1,0,0,0,0,0],[0,0,0,1,1,1,0,0],[0,0,0,0,0,0,1,1],[0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0],[1,0,0,0,0,0,0,0]];
 
-assert(JSON.stringify(depthFirstSearch(b,0,8)) == JSON.stringify([0,7,6,5,4,3,2,1]))
-
-assert(JSON.stringify(depthFirstSearch(b,1,0)) == JSON.stringify([1,2,3,4,5,6,7,0]))
-
-assert(JSON.stringify(depthFirstSearch(a,3,7)) == JSON.stringify([]))
-
-assert(JSON.stringify(depthFirstSearch(a,3,2)) == JSON.stringify([3,2]))
-
-assert(JSON.stringify(depthFirstSearch(a,0,3)) == JSON.stringify([0,1,2,3]))
+assert.deepEqual(depthFirstSearch(b,0,7), [0,1,3,2,6,7]);
+assert.deepEqual(depthFirstSearch(b,1,0), [1,3,2,6,7,0]);
+assert.deepEqual(depthFirstSearch(a,3,7), []);
+assert.deepEqual(depthFirstSearch(a,3,2), [3,2]);
+assert.deepEqual(depthFirstSearch(a,0,3), [0,1,2,3]);
