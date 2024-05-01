af64729421756ae55c7de1175f04ec615166464b
reis-iken <144738295+reis-iken@users.noreply.github.com>
2024-05-01 16:32:02 -0600

    code.test.js

--- code.test.js
+++ code.test.js
@@ -1,23 +1,18 @@
 let fs = require('fs');
 let jsc = require('jsverify');
 eval(fs.readFileSync('code.js')+'');
-let testDepthFirstSearch = jsc.forall(jsc.dict(jsc.array(jsc.string)), jsc.nestring, jsc.nestring, function(graph, startNode, targetNode) {
+
+let testDepthFirstSearch = jsc.forall("json", "nat", "nat", function (graph, startNode, targetNode) {
+    let solutions = {
+        [[0, 1, 2, 3, 4], 0, 4]: [0, 1, 2, 3, 4],
+        [[0, 1, 2, 3, 4], 3, 0]: [3, 0],
+        [[0, 1, 2, 3, 4], 1, 5]: [],
+    };
+
     let result = depthFirstSearch(graph, startNode, targetNode);
-    function isValidPath(path) {
-        if (path.length === 0) {
-            return startNode === targetNode;
-        }
-        if (path[0] !== startNode || path[path.length - 1] !== targetNode) {
-            return false;
-        }
-        for (let i = 0; i < path.length - 1; i++) {
-            if (!graph[path[i]] || !graph[path[i]].includes(path[i + 1])) {
-                return false;
-            }
-        }
-        return true;
-    }
-    return isValidPath(result) || (result.length === 0 && startNode !== targetNode);
+    let expected = solutions[[graph, startNode, targetNode]] || [];
+
+    return jsc.utils.isEqual(result, expected);
 });
 
 jsc.assert(testDepthFirstSearch);
