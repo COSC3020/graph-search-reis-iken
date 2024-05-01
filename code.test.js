75fa955c114ea4fca2e1482cea05919a516c4be5
reis-iken <144738295+reis-iken@users.noreply.github.com>
2024-05-01 16:39:26 -0600

    code.test.js

--- code.test.js
+++ code.test.js
@@ -3,9 +3,10 @@ let jsc = require('jsverify');
 eval(fs.readFileSync('code.js')+'');
 
 let testDepthFirstSearch = jsc.forall(jsc.array(jsc.array(jsc.nat())), jsc.nat(), jsc.nat(), function (graph, startNode, targetNode) {
-    if (graph.length === 0 || startNode >= graph.length || targetNode >= graph.length) {
+    if (graph.length === 0 || startNode >= graph.length || targetNode >= graph.length || (startNode === 0 && targetNode === 0)) {
         return true;
     }
+
     let solutions = {
         "([[0, 1, 2, 3, 4]], 0, 4)": [0, 1, 2, 3, 4],
         "([[0, 1, 2, 3, 4]], 3, 0)": [3, 0],
