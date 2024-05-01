1cf1239c0928a68e2952d79ea386875b3d70d3e6
reis-iken <144738295+reis-iken@users.noreply.github.com>
2024-05-01 17:13:57 -0600

    code.test.js

--- code.test.js
+++ code.test.js
@@ -1,17 +1,17 @@
 const fs = require('fs');
 const assert = require('assert');
 
-fs.readFile('code.js', 'utf8', function(err, data) {
-    if (err) throw err;
+eval(fs.readFileSync('code.js')+'');
 
-    eval(data);
+a = [[0,1,2],[3,2,1],[1,2],[1,2,3]]
+b = [[3],[6],[2,3,4],[3,4,5],[4,5,6],[5,6,7],[6,7,8],[7,0,1]]
 
-    const a = [[0,1,2],[3,2,1],[1,2],[1,2,3]];
-    const b = [[3],[6],[2,3,4],[3,4,5],[4,5,6],[5,6,7],[6,7,8],[7,0,1]];
+assert(JSON.stringify(depthFirstSearch(b,0,8)) == JSON.stringify([0,7,6,5,4,3,2,1]))
 
-    assert.deepEqual(depthFirstSearch(b,0,8), [0,7,6,5,4,3,2,1]);
-    assert.deepEqual(depthFirstSearch(b,1,0), [1,2,3,4,5,6,7,0]);
-    assert.deepEqual(depthFirstSearch(a,3,7), []);
-    assert.deepEqual(depthFirstSearch(a,3,2), [3,2]);
-    assert.deepEqual(depthFirstSearch(a,0,3), [0,1,2,3]);
-});
+assert(JSON.stringify(depthFirstSearch(b,1,0)) == JSON.stringify([1,2,3,4,5,6,7,0]))
+
+assert(JSON.stringify(depthFirstSearch(a,3,7)) == JSON.stringify([]))
+
+assert(JSON.stringify(depthFirstSearch(a,3,2)) == JSON.stringify([3,2]))
+
+assert(JSON.stringify(depthFirstSearch(a,0,3)) == JSON.stringify([0,1,2,3]))
